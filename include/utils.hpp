f24dbb596ea91a4c5118f33e85bce60ee3983f73
Connor Garey <connorgarey1999@gmail.com>
2026-04-01 20:13:49 +0100

    Added Utils and or_flag util

---
+++ include/utils.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <span>
+#include <type_traits>
+#include <utility>
+
+namespace Utils
+{
+template <typename T>
+concept EnumClass = std::is_enum_v<T>;
+
+template <EnumClass T> auto or_flags(std::span<T> flags)
+{
+  if (flags.empty())
+    return;
+
+  return [flags]()
+  {
+    auto result{0};
+    for (const auto &flag : flags)
+      result |= std::to_underlying(flag);
+
+    return result;
+  }();
+}
+} // namespace Utils
