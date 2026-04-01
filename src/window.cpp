98dbc8d246d297821677401e55984b19e1a3d215
Connor Garey <connorgarey1999@gmail.com>
2026-04-01 17:44:57 +0100

    Moved or_flags to anonymous namespace.

--- src/window.cpp
+++ src/window.cpp
@@ -1,17 +1,20 @@
 #include "SDL3/SDL_video.h"
 #include "window.hpp"
 
+namespace
+{
+auto or_flags = [](const std::span<Window::Instance::Flags> flags)
+{
+  auto result{0};
+  for (const auto &flag : flags)
+    result |= std::to_underlying(flag);
+  return result;
+};
+} // namespace
+
 Window::Instance::Instance(const std::string &title, std::pair<std::int32_t, std::int32_t> xy,
                            const std::span<Flags> flags)
     : title{title.c_str()}, xy_{xy.first, xy.second},
-      window_instance_{SDL_CreateWindow(title.c_str(), xy_.first, xy_.second,
-                                        [flags]()
-                                        {
-                                          auto result{0};
-                                          for (const auto &flag : flags)
-                                            result |= std::to_underlying(flag);
-                                          return result;
-                                        }()),
-                       &SDL_DestroyWindow}
+      window_instance_{SDL_CreateWindow(title.c_str(), xy_.first, xy_.second, or_flags(flags)), &SDL_DestroyWindow}
 {
 }
