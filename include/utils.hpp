a3beda39ca4a38a8827efe2c783645bca3800b0c
Connor Garey <connorgarey1999@gmail.com>
2026-04-01 20:26:24 +0100

    Use an OrType for number and error handling.
    .

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <expected>
 #include <span>
 #include <type_traits>
 #include <utility>
@@ -9,14 +10,21 @@ namespace Utils
 template <typename T>
 concept EnumClass = std::is_enum_v<T>;
 
-template <EnumClass T> auto or_flags(std::span<T> flags)
+template <typename T>
+concept Number = std::is_integral_v<T>;
+
+enum class OrErrors
+{
+  FLAG_SET_EMPTY,
+};
+template <EnumClass FlagSet, Number OrType> std::expected<OrType, OrErrors> or_flags(std::span<FlagSet> flags) noexcept
 {
   if (flags.empty())
-    return;
+    return std::unexpected(OrErrors::FLAG_SET_EMPTY);
 
   return [flags]
   {
-    auto result{0};
+    OrType result{0};
     for (const auto &flag : flags)
       result |= std::to_underlying(flag);
 
