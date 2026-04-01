93169090f00a8f2d6270c642554093c2db195d13
Connor Garey <connorgarey1999@gmail.com>
2026-04-01 23:56:08 +0100

    Formatting.

--- src/init.cpp
+++ src/init.cpp
@@ -1,14 +1,21 @@
+#include "SDL3/SDL_init.h"
 #include "init.hpp"
 #include "utils.hpp"
-#include "SDL3/SDL_init.h"
 
-Init::Instance::Instance(std::span<Flags> flags)
+#include <cassert>
+
+Init::Instance::Instance(std::span<Flags> flags, const Metadata &app_data)
 {
-  const auto init_result {Utils::or_flags<Flags, SDL_InitFlags>(flags)};
-    SDL_Init(init_result);   
+  const auto init_result{Utils::or_flags<Flags, SDL_InitFlags>(flags)};
+  SDL_Init(init_result);
 }
 
-Init::Instance::~Instance()
+Init::Instance::~Instance() { SDL_Quit(); }
+
+void Init::Instance::set_metadata(const Metadata &app_data) const noexcept
 {
-  SDL_Quit();
+  for (const auto &str : {app_data.name, app_data.version, app_data.identifier})
+    assert(!str.empty());
+
+  SDL_SetAppMetadata(app_data.name.c_str(), app_data.version.c_str(), app_data.identifier.c_str());
 }
