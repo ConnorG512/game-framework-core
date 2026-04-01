009e70f7c6be91afcc0ab397e8d8fad72eabff32
Connor Garey <connorgarey1999@gmail.com>
2026-04-01 17:37:55 +0100

    Foramtting.

--- src/window.cpp
+++ src/window.cpp
@@ -1,12 +1,17 @@
-#include "window.hpp"
 #include "SDL3/SDL_video.h"
+#include "window.hpp"
 
-Window::Instance::Instance(const std::string& title, std::pair<std::int32_t, std::int32_t> xy, const std::span<Flags> flags) 
-  : title {title.c_str()}
-  , xy_ {xy.first, xy.second}
-  , window_instance_ {SDL_CreateWindow(title.c_str(), xy_.first, xy_.second, [flags](){
-      auto result {0};
-      for(const auto& flag : flags)
-        result |= std::to_underlying(flag);
-      return result;
-      }()), &SDL_DestroyWindow} {}
+Window::Instance::Instance(const std::string &title, std::pair<std::int32_t, std::int32_t> xy,
+                           const std::span<Flags> flags)
+    : title{title.c_str()}, xy_{xy.first, xy.second},
+      window_instance_{SDL_CreateWindow(title.c_str(), xy_.first, xy_.second,
+                                        [flags]()
+                                        {
+                                          auto result{0};
+                                          for (const auto &flag : flags)
+                                            result |= std::to_underlying(flag);
+                                          return result;
+                                        }()),
+                       &SDL_DestroyWindow}
+{
+}
