05bbc7890379ee7a7976821d29051f34b31d56d3
Connor Garey <connorgarey1999@gmail.com>
2026-04-02 23:59:54 +0100

    Added Logging class.

---
+++ include/logging.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <expected>
+#include <fstream>
+#include <string_view>
+
+namespace GFC::Logger
+{
+class Instance
+{
+public:
+  enum class InfoType
+  {
+    ERROR,
+    WARNING,
+    DEBUG,
+    INFO,
+  };
+  [[nodiscard]] std::expected<void, std::string_view> write_to_logger(const std::string &message,
+                                                                      InfoType prefix_type) noexcept;
+
+private:
+  std::ofstream file_{"application.log"};
+};
+} // namespace GFC::Logger
---
+++ src/logging.cpp
@@ -0,0 +1,33 @@
+#include "logging.hpp"
+#include <expected>
+#include <format>
+#include <print>
+
+[[nodiscard]] std::expected<void, std::string_view>
+GFC::Logger::Instance::write_to_logger(const std::string &message, InfoType prefix_type) noexcept
+{
+  const std::string_view prefix = [prefix_type] -> std::string_view
+  {
+    switch (prefix_type)
+    {
+      case InfoType::ERROR:
+        return "ERROR";
+      case InfoType::WARNING:
+        return "WARNING";
+      case InfoType::DEBUG:
+        return "DEBUG";
+      case InfoType::INFO:
+        return "INFO";
+      default:
+        return "N/A";
+    }
+  }();
+
+  if (!file_.is_open())
+    return std::unexpected("Log file is not open for writing!");
+  else
+  {
+    std::println(file_, "{}: {}", prefix, message);
+    return {};
+  }
+}
