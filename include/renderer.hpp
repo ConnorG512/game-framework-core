38dd9fa23bba03f4b241d2b12a0c038b919cb5f0
Connor Garey <connorgarey1999@gmail.com>
2026-04-03 11:06:01 +0100

    Draw now takes a vec4
    .

--- include/renderer.hpp
+++ include/renderer.hpp
@@ -1,6 +1,7 @@
-#pragma once 
+#pragma once
 
 #include "SDL3/SDL_render.h"
+#include "math/vec.hpp"
 
 #include <memory>
 
@@ -14,8 +15,9 @@ class Instance
 {
 public:
   explicit Instance(SDL_Window &window);
-  
-  void draw() noexcept;
+
+  void draw(const Math::Vec4<float> &colors) noexcept;
+
 private:
   SDL_Window &current_window_;
   std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> renderer_{SDL_CreateRenderer(&current_window_, nullptr),
--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,14 +1,15 @@
-#include "renderer.hpp"
 #include "SDL3/SDL_render.h"
 #include "SDL3/SDL_video.h"
+#include "renderer.hpp"
 
 GFC::Renderer::Instance::Instance(SDL_Window &window)
-  : current_window_(window)
-  , renderer_(SDL_CreateRenderer(&window, nullptr), &SDL_DestroyRenderer) {}
-
-void GFC::Renderer::Instance::draw() noexcept
+    : current_window_(window), renderer_(SDL_CreateRenderer(&window, nullptr), &SDL_DestroyRenderer)
 {
-  SDL_SetRenderDrawColorFloat(renderer_.get(), 0.5f, 0.5f, 0.5f, 1.0f);
+}
+
+void GFC::Renderer::Instance::draw(const Math::Vec4<float> &colors) noexcept
+{
+  SDL_SetRenderDrawColorFloat(renderer_.get(), colors.x, colors.y, colors.z, colors.w);
   SDL_RenderClear(renderer_.get());
   SDL_RenderPresent(renderer_.get());
 }
