d0505acadd97509ed5592d6076d18b24bb0fed91
Connor Garey <connorgarey1999@gmail.com>
2026-04-03 12:50:35 +0100

    Assert to ensure that the float values are within range.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,5 +1,6 @@
 #include "SDL3/SDL_render.h"
 #include "SDL3/SDL_video.h"
+#include <cassert>
 #include "renderer.hpp"
 
 GFC::Renderer::Instance::Instance(SDL_Window &window)
@@ -9,6 +10,9 @@ GFC::Renderer::Instance::Instance(SDL_Window &window)
 
 void GFC::Renderer::Instance::draw(const Math::Vec4<float> &colors) noexcept
 {
+  for(const auto& color : {colors.x, colors.y, colors.z, colors.w})
+    assert(color >= 0.0f && color <= 1.0f);
+
   SDL_SetRenderDrawColorFloat(renderer_.get(), colors.x, colors.y, colors.z, colors.w);
   SDL_RenderClear(renderer_.get());
   SDL_RenderPresent(renderer_.get());
