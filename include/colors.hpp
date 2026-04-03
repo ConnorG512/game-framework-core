3a6cae1a18242f889aa9c01531d1bf0539b76aad
Connor Garey <connorgarey1999@gmail.com>
2026-04-03 12:33:16 +0100

    Added preset colours

---
+++ include/colors.hpp
@@ -0,0 +1,13 @@
+#pragma once 
+
+#include "math/vec.hpp"
+
+namespace GFC::Colors 
+{
+  constexpr inline Math::Vec4<float> MIDDLE_GREY {0.5f, 0.5f, 0.5f, 1.0f};
+  constexpr inline Math::Vec4<float> WHITE {1.0f, 1.0f, 1.0f, 1.0f};
+  constexpr inline Math::Vec4<float> BLACK {0.0f, 0.0f, 0.0f, 1.0f};
+  constexpr inline Math::Vec4<float> RED {1.0f, 0.0f, 0.0f, 1.0f};
+  constexpr inline Math::Vec4<float> GREEN {0.0f, 1.0f, 0.0f, 1.0f};
+  constexpr inline Math::Vec4<float> BLUE {0.0f, 0.0f, 1.0f, 1.0f};
+}
