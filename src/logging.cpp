2a0f98b0ac0c6a417a782808effb630c703c53fa
Connor Garey <connorgarey1999@gmail.com>
2026-04-05 13:37:05 +0100

    Moved getting the prefix to an anonymous function.

--- src/logging.cpp
+++ src/logging.cpp
@@ -3,22 +3,42 @@
 #include <expected>
 #include <format>
 #include <print>
+#include <string_view>
 #include <utility>
 
-namespace {
-  bool is_at_max_log_count(std::uint64_t current_log_count, std::uint64_t max_log_count)
-  {
-    // 0 indicates no limit. 
-    if(max_log_count == 0)
-      return false; 
-    
-    return (current_log_count >= max_log_count);
-  }
+namespace
+{
+[[nodiscard]] bool is_at_max_log_count(std::uint64_t current_log_count, std::uint64_t max_log_count)
+{
+  // 0 indicates no limit.
+  if (max_log_count == 0)
+    return false;
+
+  return (current_log_count >= max_log_count);
 }
 
-GFC::Logger::Instance::Instance(const std::string &file_path, const std::span<const LogType> active_log_types) 
-  : file_(file_path) 
-  , selected_types_(GFC::Bitset::create_bitmask(active_log_types)) {}
+[[nodiscard]] std::string_view get_logging_prefix(GFC::Logger::LogType prefix_type)
+{
+  switch (prefix_type)
+  {
+    case GFC::Logger::LogType::ERROR:
+      return "ERROR";
+    case GFC::Logger::LogType::WARNING:
+      return "WARNING";
+    case GFC::Logger::LogType::DEBUG:
+      return "DEBUG";
+    case GFC::Logger::LogType::INFO:
+      return "INFO";
+    default:
+      return "N/A";
+  }
+}
+} // namespace
+
+GFC::Logger::Instance::Instance(const std::string &file_path, const std::span<const LogType> active_log_types)
+    : file_(file_path), selected_types_(GFC::Bitset::create_bitmask(active_log_types))
+{
+}
 
 [[nodiscard]] std::expected<void, std::string_view> GFC::Logger::Instance::write_to_logger(const std::string &message,
                                                                                            LogType prefix_type) noexcept
@@ -29,28 +49,11 @@ GFC::Logger::Instance::Instance(const std::string &file_path, const std::span<co
   if (!GFC::Bitset::is_active_bit(selected_types_, prefix_type))
     return {};
 
-  const std::string_view prefix = [prefix_type] -> std::string_view
-  {
-    switch (prefix_type)
-    {
-      case LogType::ERROR:
-        return "ERROR";
-      case LogType::WARNING:
-        return "WARNING";
-      case LogType::DEBUG:
-        return "DEBUG";
-      case LogType::INFO:
-        return "INFO";
-      default:
-        return "N/A";
-    }
-  }();
-
   if (!file_.is_open())
     return std::unexpected("Log file is not open for writing!");
   else
   {
-    std::println(file_, "{}: {}", prefix, message);
+    std::println(file_, "{}: {}", get_logging_prefix(prefix_type), message);
     return {};
   }
 }
