a0f327c97257b20140027abc525a181c4c0790f9
Connor Garey <connorgarey1999@gmail.com>
2026-04-07 15:00:34 +0100

    New filesystem class

--- include/filesystem.hpp
+++ include/filesystem.hpp
@@ -1,12 +1,17 @@
 #pragma once
 
-#include "logging.hpp"
-#include <functional>
+#include <filesystem>
 #include <string>
 
 namespace GFC::Filesystem
 {
-[[nodiscard]] bool
-does_directory_exist(const std::string &file_path,
-                     std::function<void(const std::string &message, GFC::Logger::LogType type)> log_func = nullptr);
-}
+class Instance
+{
+public:
+  explicit Instance(const std::string &program_dir_name);
+  const std::string &get_directory_path() const noexcept;
+
+private:
+  std::filesystem::path program_dir_path_{};
+};
+} // namespace GFC::Filesystem
--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -1,16 +1,14 @@
 #include "filesystem.hpp"
 
 #include <cassert>
+#include <cstdlib>
 #include <filesystem>
+#include <format>
 
-[[nodiscard]] bool GFC::Filesystem::does_directory_exist(
-    const std::string &file_path, std::function<void(const std::string &message, GFC::Logger::LogType type)> log_func)
+GFC::Filesystem::Instance::Instance(const std::string &program_dir_name)
+    : program_dir_path_(std::format("{}/.local/share/{}/", std::getenv("HOME"), program_dir_name))
 {
-  const char *err_dir_empty{"File path that has been provided to \"does_directory_exist\" is empty!"};
-  assert(!file_path.empty() && err_dir_empty);
-
-  if (log_func != nullptr)
-    log_func(err_dir_empty, GFC::Logger::LogType::ERROR);
-
-  return (std::filesystem::exists(file_path));
+  assert(!program_dir_path_.empty());
+  if (!std::filesystem::exists(program_dir_path_))
+    std::filesystem::create_directory(program_dir_path_);
 }
