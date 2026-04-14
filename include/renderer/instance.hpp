6ddf13985c8d4a78eb07cdc6e5efd97b409d56fe
Connor Garey <connorgarey1999@gmail.com>
2026-04-14 17:11:11 +0100

    Loader shader from directory implementation.

--- include/renderer/instance.hpp
+++ include/renderer/instance.hpp
@@ -1,10 +1,32 @@
-#pragma once 
+#pragma once
 
-namespace GFC {
-  namespace Logger {class Instance;}
-  namespace EasyWindow {class Instance;}
-}
+#include <expected>
+#include <filesystem>
+#include <vulkan/vulkan_core.h>
 
-namespace GFC::Ren {
-  void init(const EasyWindow::Instance& window, Logger::Instance* logger = nullptr);
+namespace GFC
+{
+namespace Logger
+{
+class Instance;
 }
+namespace EasyWindow
+{
+class Instance;
+}
+} // namespace GFC
+
+namespace GFC::Ren
+{
+void init(const EasyWindow::Instance &window, Logger::Instance *logger = nullptr);
+
+enum class ShaderLoadError
+{
+  DIRECTORY_NOT_FOUND,
+  FILE_NOT_OPEN,
+  FAILED_TO_CREATE_SHADER_MODULE,
+};
+
+[[nodiscard]] std::expected<VkShaderModule, ShaderLoadError>
+load_shader(const std::filesystem::path &path, VkDevice device, GFC::Logger::Instance *logger = nullptr);
+} // namespace GFC::Ren
--- src/renderer/instance.cpp
+++ src/renderer/instance.cpp
@@ -1,10 +1,16 @@
-#include "renderer/instance.hpp"
 #include "logging.hpp"
+#include "renderer/instance.hpp"
 
 #include <VkBootstrap.h>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <filesystem>
 #include <format>
+#include <fstream>
 #include <stdexcept>
 #include <string>
+#include <vulkan/vulkan_core.h>
 
 void GFC::Ren::init(const EasyWindow::Instance &window, Logger::Instance *logger)
 {
@@ -14,13 +20,50 @@ void GFC::Ren::init(const EasyWindow::Instance &window, Logger::Instance *logger
                       .use_default_debug_messenger()
                       .require_api_version(1, 3, 0)
                       .build();
-  if(!inst_ret.has_value())
+  if (!inst_ret.has_value())
   {
-    const std::string vulkan_build_error {std::format("Failed to build Vulkan instance! Error: {}", inst_ret.error().message())};
-    if(logger != nullptr)
+    const std::string vulkan_build_error{
+        std::format("Failed to build Vulkan instance! Error: {}", inst_ret.error().message())};
+    if (logger != nullptr)
       logger->write_to_logger(vulkan_build_error, GFC::Logger::LogType::ERROR);
     throw std::runtime_error(vulkan_build_error);
   }
 
   vkb::Instance vkb_inst = inst_ret.value();
+}
+
+[[nodiscard]] std::expected<VkShaderModule, GFC::Ren::ShaderLoadError>
+GFC::Ren::load_shader(const std::filesystem::path &path, VkDevice device, GFC::Logger::Instance *logger)
+{
+  if (!std::filesystem::exists(path))
+    return std::unexpected(GFC::Ren::ShaderLoadError::DIRECTORY_NOT_FOUND);
+
+  std::ifstream open_shader_file(path, std::ios::ate | std::ios::binary);
+  if (!open_shader_file.is_open())
+    return std::unexpected(GFC::Ren::ShaderLoadError::FILE_NOT_OPEN);
+
+  const std::size_t file_size{static_cast<std::size_t>(open_shader_file.tellg())};
+  std::vector<std::uint32_t> shader_buffer(file_size / sizeof(std::uint32_t));
+  assert(file_size != 0 && !shader_buffer.empty());
+
+  if (logger != nullptr)
+    logger->write_to_logger(
+        std::format("Shader file successfully opened at path {} with size {}.", path.string(), file_size),
+        Logger::LogType::DEBUG);
+
+  open_shader_file.seekg(0);
+  open_shader_file.read(reinterpret_cast<char *>(shader_buffer.data()), file_size);
+
+  const VkShaderModuleCreateInfo createInfo{
+      .sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
+      .pNext = nullptr,
+      .codeSize = shader_buffer.size() * sizeof(std::uint32_t),
+      .pCode = shader_buffer.data(),
+  };
+  VkShaderModule shader_module{};
+
+  if (vkCreateShaderModule(device, &createInfo, nullptr, &shader_module) != VK_SUCCESS)
+    return std::unexpected(ShaderLoadError::FAILED_TO_CREATE_SHADER_MODULE);
+
+  return shader_module;
 }
